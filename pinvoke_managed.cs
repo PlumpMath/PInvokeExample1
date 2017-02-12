4e7a8e886310f574308dc16509ac9fa52bff0a8b
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2017-02-12 21:09:21 +0900

    intの受け渡しのサンプルを追加しました。

---
+++ pinvoke_managed.cs
@@ -0,0 +1,82 @@
+﻿// 参考:
+// Interop with Native Libraries | Mono
+// http://www.mono-project.com/docs/advanced/pinvoke/
+
+using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	struct S
+	{
+		public int n;
+		public byte b;
+	}
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	class C
+	{
+		public int n;
+		public byte b;
+	}
+
+	[DllImport("pinvoke_native")]
+	static extern void Func0();
+	[DllImport("pinvoke_native")]
+	static extern void Func1(int n);
+	[DllImport("pinvoke_native")]
+	static extern void Func2(ref int n);
+	[DllImport("pinvoke_native")]
+	static extern void Func3(out int n);
+	[DllImport("pinvoke_native")]
+	static extern void Func4([Out] int n);
+	[DllImport("pinvoke_native")]
+	static extern void Func5(IntPtr n);
+
+	static void Main(string[] args)
+	{
+		Func0();
+
+		{
+			int n = 123;
+			Func1(n);
+			Console.WriteLine("Managed: n = " + n);
+		}
+
+		{
+			int n = 123;
+			Func2(ref n);
+			Console.WriteLine("Managed: n = " + n);
+		}
+
+		{
+			int n = 123;
+			Func3(out n);
+			Console.WriteLine("Managed: n = " + n);
+		}
+
+		{
+			int n = 123;
+			Func4(n);
+			Console.WriteLine("Managed: n = " + n);
+		}
+
+		{
+			var n = 123;
+			var ptr = new IntPtr();
+			var sz = Marshal.SizeOf(typeof(int));
+			ptr = Marshal.AllocCoTaskMem(sz);
+			Marshal.WriteInt32(ptr, n);
+			Func5(ptr);
+			n = Marshal.ReadInt32(ptr);
+			Console.WriteLine("Managed: n = " + n);
+		}
+	}
+}
+
+// Local variables:
+// mode: csharp
+// coding: utf-8-with-signature
+// indent-tabs-mode: t
+// tab-width: 4
+// End:
---
+++ pinvoke_native.cc
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+#define DLL_EXPORT extern "C"
+
+struct S {
+	int n;
+	uint8_t b;
+};
+class C {
+	int n;
+	uint8_t b;
+};
+
+DLL_EXPORT void Func0()
+{
+	std::cout << "Native: " << __PRETTY_FUNCTION__ << std::endl;
+}
+
+DLL_EXPORT void Func1(int n)
+{
+	std::cout << "Native: " << __PRETTY_FUNCTION__ << std::endl;
+	std::cout << "  n = " << n << std::endl;
+	n = 456;
+	std::cout << "    => " << n << std::endl;
+}
+
+DLL_EXPORT void Func2(int *p)
+{
+	std::cout << "Native: " << __PRETTY_FUNCTION__ << std::endl;
+	std::cout << "  p = " << p << std::endl;
+	std::cout << "  *p = " << *p << std::endl;
+	if (p != nullptr) {
+		*p = 456;
+		std::cout << "    => " << *p << std::endl;
+	}
+}
+
+DLL_EXPORT void Func3(int *p)
+{
+	std::cout << "Native: " << __PRETTY_FUNCTION__ << std::endl;
+	std::cout << "  p = " << p << std::endl;
+	std::cout << "  *p = " << *p << std::endl;
+	if (p != nullptr) {
+		*p = 456;
+		std::cout << "    => " << *p << std::endl;
+	}
+}
+
+DLL_EXPORT void Func4(int n)
+{
+	std::cout << "Native: " << __PRETTY_FUNCTION__ << std::endl;
+	std::cout << "  n = " << n << std::endl;
+	n = 456;
+	std::cout << "    => " << n << std::endl;
+}
+
+DLL_EXPORT void Func5(int *p)
+{
+	std::cout << "Native: " << __PRETTY_FUNCTION__ << std::endl;
+	std::cout << "  p = " << p << std::endl;
+	std::cout << "  *p = " << *p << std::endl;
+	if (p != nullptr) {
+		*p = 456;
+		std::cout << "    => " << *p << std::endl;
+	}
+}
+
+// Local variables:
+// mode: c++
+// coding: utf-8-with-signature
+// c-file-style: "bsd"
+// c-basic-offset: 4
+// c-file-offsets: ((inextern-lang . 0)(innamespace . 0))
+// indent-tabs-mode: t
+// tab-width: 4
+// End:
